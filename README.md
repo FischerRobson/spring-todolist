e183f65b465ccab2a2c2d13b4d97e41390d2519e
Robson Fischer <fischerrobson@gmail.com>
2023-10-10 18:24:31 -0300

    db integration with JPA/H2 mem db, user repository

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # To do list
 
 This API was built with Java + Spring boot
+
+## Open database console
+
+`http://localhost:8080/h2-console`
--- src/main/java/br/com/fischer/todolist/user/UserController.java
+++ src/main/java/br/com/fischer/todolist/user/UserController.java
@@ -1,17 +1,25 @@
 package br.com.fischer.todolist.user;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/users")
 public class UserController {
 
-    @PostMapping()
-    public void create(@RequestBody User user) {
+    @Autowired
+    private UserRepository userRepository;
 
+    @PostMapping()
+    public ResponseEntity create(@RequestBody User user) {
+        User userWithUsername = this.userRepository.findByUsername(user.getUsername());
+        if (userWithUsername != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+        }
+        User savedUser = this.userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
 }
